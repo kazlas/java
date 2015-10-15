9a71ddf9bc1acd6624f7d74d1d5dc1effb54c667
kazlas <kazlas@users.noreply.github.com>
2015-10-15 10:23:26 +0200

    Make it EJB - so it can have access to other EJB on server.

--- simple-json-ejb/src/main/java/pl/kazlas/microservices/MyResource.java
+++ simple-json-ejb/src/main/java/pl/kazlas/microservices/MyResource.java
@@ -4,10 +4,12 @@ package pl.kazlas.microservices;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ejb.Stateless;
 
 /** Example resource class hosted at the URI path "/myresource"
  */
 @Path("/myresource")
+@Stateless
 public class MyResource {
     
     /** Method processing HTTP GET requests, producing "text/plain" MIME media
