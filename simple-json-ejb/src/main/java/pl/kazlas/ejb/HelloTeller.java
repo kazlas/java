fb9ecf15b99d608b405dd942315b5c22c8b66498
kazlas <kazlas@users.noreply.github.com>
2016-04-19 20:40:47 +0200

    Remove static variable from Hello Teller EJB

--- simple-json-ejb/src/main/java/pl/kazlas/ejb/HelloTeller.java
+++ simple-json-ejb/src/main/java/pl/kazlas/ejb/HelloTeller.java
@@ -1,13 +1,11 @@
 package pl.kazlas.ejb;
 
-import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 
 @Stateless
 public class HelloTeller {
-	private static final String helloMessage = "Hello form EJB!";
 
-	public String sayHallo (){
-    	return helloMessage;		
+	public String sayHallo() {
+		return "Hello from EJB!";
 	}
 }
